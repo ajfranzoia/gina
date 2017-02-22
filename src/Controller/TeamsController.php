33ace758a32c3042ea7ba790f1bb05d852d33419
Augusto Franzoia <ajfranzoia@gmail.com>
2017-02-22 09:40:27 -0300

    Create TeamFavorite model and add related TeamsController methods
    Creates the model representation for a favorite team and adds
    the addFavorite() and removeFavorite() methods in TeamsController.
    Refactored API calls using Lib\FootballDataApi utility class.

--- src/Controller/TeamsController.php
+++ src/Controller/TeamsController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use Gina\Controller;
+use Gina\Utils;
+use App\Model\TeamFavorite;
+use App\Lib\FootballDataApi;
 
 /**
  * Teams controller
@@ -16,31 +19,70 @@ class TeamsController extends Controller {
 	 * @return mixed
 	 */
 	public function index($request) {
-		$leagueId = $this->config->get('footbalData')['leagueId'];
-
-		$client = new \GuzzleHttp\Client();
-		$res = $client->request('GET', "http://api.football-data.org/v1/competitions/$leagueId/teams", [
-			'headers' => [
-				'X-Auth-Token' => $this->config->get('footbalData')['authToken']
-			]
-		]);
-		$result = $res->getBody()->getContents();
+		$footbalDataConfig = $this->config->get('footbalData');
+		$api = new FootballDataApi($footbalDataConfig);
 
 		return $this->render([
-			'teams' => json_decode($result, true)['teams']
+			'teams' => $api->getTeams(),
+			'favorites' => TeamFavorite::find('all')
 		]);
 	}
 
 	/**
-	 * Action that shows data for a selected team
+	 * Action that adds a new favorite team
 	 *
 	 * @param  Request $request
+	 * @param  string $teamId
 	 * @return mixed
 	 */
-	public function view($id) {
-		return $this->respondJson([
-			'name' => 'Arsenal FC'
+	public function addFavorite($request, $teamId = null) {
+		if (!$request->isMethod('post')) {
+			throw new \Exception('This action requires POST', 400);
+		}
+
+		if (!$teamId) {
+			throw new \Exception('Missing team id', 400);
+		}
+
+		$favorite = TeamFavorite::find_by_team_id($teamId);
+
+		if ($favorite) {
+			return $this->respondJson(['result' => true]);
+		}
+
+		TeamFavorite::create([
+			'team_id' => $teamId,
+			'assigned' => new \DateTime()
 		]);
+
+		return $this->respondJson(['result' => true]);
+	}
+
+	/**
+	 * Action that removes an existent favorite team
+	 *
+	 * @param  Request $request
+	 * @param  string $teamId
+	 * @return mixed
+	 */
+	public function removeFavorite($request, $teamId = null) {
+		if (!$request->isMethod('post')) {
+			throw new \Exception('This action requires POST', 400);
+		}
+
+		if (!$teamId) {
+			throw new \Exception('Missing team id', 400);
+		}
+
+		$favorite = TeamFavorite::find_by_team_id($teamId);
+
+		if (!$favorite) {
+			return $this->respondJson(['result' => true]);
+		}
+
+		$favorite->delete();
+
+		return $this->respondJson(['result' => true]);
 	}
 
 }
---
+++ src/Model/TeamFavorite.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use Gina\Model;
+
+/**
+ * Favorite teams model representation
+ */
+class TeamFavorite extends Model {
+
+	/**
+	 * Table name in database
+	 *
+	 * @var string
+	 */
+    static $table_name = 'teams_favorites';
+
+}
